6df1dc2cbb9c053fc44155eb7393aea70eb2513a
Jorge Estigarribia <jorgestiga@hotmail.com>
2018-11-28 18:41:59 +0100

    Added assignment 1 session14.

---
+++ session14/src/main/java/Assignment1.java
@@ -0,0 +1,14 @@
+
+/* Assignment 1
+Convert the Optional by using the methods provided by the Optional class. Implement the following computations (in the given order):
+    - check if value is of type Integer
+    - calculate the power of two for the value
+    - check if the value is bigger than 400
+    - convert the value to true if it is bigger than 200 and to false if it is less than or equal to 200
+    - get the value of the optional if available and print it out
+    - if no value is present print out "out of range"
+ */
+
+public class Assignment1 {
+    
+}
