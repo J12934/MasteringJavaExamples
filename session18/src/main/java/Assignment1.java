5660786c9c7e356d7f193d762b5ce0c34a0c130d
Jorge Estigarribia <jorgestiga@hotmail.com>
2018-12-12 19:24:23 +0100

    Added first attempt of the session18.

---
+++ session18/src/main/java/Assignment1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Assignment1 {
+
+    // Thread to check if the number is a prime number
+    Thread thread1 = new Thread(()->{
+        for (int i = 0; i<=10000; i++){
+            if number
+        }
+        System.out.println("");
+    });
+
+    // Thread to calculate the next lower fibonacci numebr
+    Thread thread1 = new Thread(()->{
+        for (int i = 0; i<=10000; i++){
+            if number
+        }
+        System.out.println("");
+    });
+
+    // Thread to calculate the closest 2er potency
+    Thread thread1 = new Thread(()->{
+        for (int i = 0; i<=10000; i++){
+            if number
+        }
+        System.out.println("");
+    });
+
+    public static void main(String[] args){
+        System.out.println("Type in a number between 0 and 10000:");
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+
+        System.out.println("");
+        //join all threads
+        //Counter with count of three
+        //
+    }
+}
